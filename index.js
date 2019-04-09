6788910801d9859aaf61c4526eaf7cc930715ec5
Pavel Kuzmin <mpakunderscore@gmail.com>
2019-04-09 17:31:09 +0300

    Some brain relaxation process

---
+++ index.js
@@ -0,0 +1,31 @@
+let postfix = '';
+
+let counter = -5;
+
+let name = '.space';
+
+function typesetting() {
+
+    if (counter > 0) {
+
+        postfix = name.slice(0, counter) + '_';
+
+        if (counter > 5)
+            counter = -5;
+
+    } else {
+
+        if (postfix === '')
+            postfix = '_';
+        else
+            postfix = '';
+    }
+
+    document.getElementById('typesetting-last').innerText = postfix;
+
+    counter++;
+}
+
+typesetting();
+
+let timer = setInterval(typesetting, 500);
